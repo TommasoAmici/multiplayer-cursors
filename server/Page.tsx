09d4ad4521bb9608d938eed03d40860b3e8d7a30
Tommaso Amici <me@tommasoamici.com>
2024-07-08 10:10:27 +0200

    refactor: move cursor svg to its own file

--- server/Page.tsx
+++ server/Page.tsx
@@ -1,3 +1,5 @@
+import { CursorTemplate } from "./CursorTemplate";
+
 export function Page() {
   return (
     <html>
@@ -7,18 +9,8 @@ export function Page() {
         <script type="module" src="/client/index.ts"></script>
       </head>
       <body>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          width="20"
-          height="20"
-          className="hidden"
-          id="cursor"
-        >
-          <path d="m13.9 12.36 3.1 8.5-2.82 1.02-3.09-8.5-4.17 3.16L8.4 1.64 19.13 12.1l-5.22.26Z" />
-        </svg>
         <h1>Multiplayer Cursors</h1>
+        <CursorTemplate />
       </body>
     </html>
   );
